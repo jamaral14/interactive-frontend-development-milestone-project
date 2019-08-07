fe700354233d8f5156ec823ff80d7a07f8d1f059
Ubuntu <ubuntu@ip-172-31-82-15.ec2.internal>
2019-08-07 19:54:00 +0000

    added event

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
- /* Create a list that holds all of your cards*/
+/* cards array holds all cards */
 
 var card = document.querySelectorAll('.card');
 console.log(card);
+var cards = document.getElementsByClassName('card');
+console.log(cards);
+
+cards.addEventListener("click", function () {
+    cards.classList.toggle("open", "show");
+});
 
 var cards = [... card];
 console.log(cards[12]);
 
+function shuffle(array) {
+
+
+    return array; 
+}
+
+/* set up the event listener for a card. */ 
+
+ cards.onclick = function(event){
+     this.classList.toggle('open');
+     this.classList.toggle('show');
+
+ } 
