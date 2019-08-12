fb992608fd4529809e9118ba1435bc43a66779d7
Ubuntu <ubuntu@ip-172-31-82-15.ec2.internal>
2019-08-12 19:43:03 +0000

    added congratulations/startTimer/and moveCounter

--- js/script.js
+++ js/script.js
@@ -1,27 +1,24 @@
-// cards array holds all cards
 let card = document.getElementsByClassName("card");
 let cards = [...card];
 
-const deck = document.getElementById("card-deck");
-
 let moves = 0;
-let counter = document.querySelector(".moves");
 
+let counter = document.querySelector(".moves");
 
 let matchedCard = document.getElementsByClassName("match");
 
+let closeicon = document.querySelector(".close");
 
- let closeicon = document.querySelector(".close");
-
-
- let modal = document.getElementById("popup1")
+let modal = document.getElementById("popup1");
 
 
 var openedCards = [];
 
+const deck = document.getElementById("card-deck");
 
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -32,68 +29,66 @@ function shuffle(array) {
     }
 
     return array;
-};
+}
 
 
 document.body.onload = startGame();
 
 
-function startGame(){
- 
- 
+
+function startGame() {
+
+
     openedCards = [];
 
 
     cards = shuffle(cards);
-   
-    for (var i = 0; i < cards.length; i++){
+
+    for (var i = 0; i < cards.length; i++) {
         deck.innerHTML = "";
         [].forEach.call(cards, function(item) {
             deck.appendChild(item);
         });
         cards[i].classList.remove("show", "open", "match", "disabled");
     }
-   
+    // reset moves
     moves = 0;
     counter.innerHTML = moves;
-    // reset rating
-    for (var i= 0; i < stars.length; i++){
-        stars[i].style.color = "#FFD700";
-        stars[i].style.visibility = "visible";
-    }
-  
-    second = 0;
-    minute = 0; 
-    hour = 0;
-    var timer = document.querySelector(".timer");
-    timer.innerHTML = "0 mins 0 secs";
-    clearInterval(interval);
 }
+//reset timer
+second = 0;
+minute = 0;
+var timer = document.querySelector(".timer");
+timer.innerHTML = "0 mins 0 secs";
+clearInterval(interval);
 
 
 
 
-var displayCard = function (){
+var displayCard = function() {
     this.classList.toggle("open");
     this.classList.toggle("show");
+    this.classList.toggle("disabled");
 };
 
 
 function cardOpen() {
     openedCards.push(this);
     var len = openedCards.length;
-    if(len === 2){
+    if (len === 2) {
         moveCounter();
-        if(openedCards[0].type === openedCards[1].type){
+        if (openedCards[0].type === openedCards[1].type) {
             matched();
-        } else {
+        }
+        else {
             unmatched();
         }
     }
-};
+}
 
 
-function matched(){
+
+function matched() {
     openedCards[0].classList.add("match", "disabled");
     openedCards[1].classList.add("match", "disabled");
     openedCards[0].classList.remove("show", "open", "no-event");
@@ -103,61 +98,125 @@ function matched(){
 
 
 
-function unmatched(){
+function unmatched() {
     openedCards[0].classList.add("unmatched");
     openedCards[1].classList.add("unmatched");
     disable();
-    setTimeout(function(){
-        openedCards[0].classList.remove("show", "open", "no-event","unmatched");
-        openedCards[1].classList.remove("show", "open", "no-event","unmatched");
+    setTimeout(function() {
+        openedCards[0].classList.remove("show", "open", "no-event", "unmatched");
+        openedCards[1].classList.remove("show", "open", "no-event", "unmatched");
         enable();
         openedCards = [];
-    },1100);
+    }, 1100);
 }
 
 
 
-function disable(){
-    Array.prototype.filter.call(cards, function(card){
+function disable() {
+    Array.prototype.filter.call(cards, function(card) {
         card.classList.add('disabled');
     });
 }
 
 
 
-function enable(){
-    Array.prototype.filter.call(cards, function(card){
+function enable() {
+    Array.prototype.filter.call(cards, function(card) {
         card.classList.remove('disabled');
-        for(var i = 0; i < matchedCard.length; i++){
+        for (var i = 0; i < matchedCard.length; i++) {
             matchedCard[i].classList.add("disabled");
         }
     });
 }
 
 
-for (var i = 0; i < cards.length; i++){
-   for (var i = 0; i < cards.length; i++){
-    card = cards[i];
-    card.addEventListener("click", displayCard);
-    card.addEventListener("click", cardOpen);
-    card.addEventListener("click",congratulations);
-};
 
-var cardFunction = function (){
-    this.classList.toggle("open");
-    this.classList.toggle("show");
-};
+function moveCounter() {
+    moves++;
+    counter.innerHTML = moves;
+    //start timer on first click
+    if (moves == 1) {
+        second = 0;
+        minute = 0;
+        startTimer();
+    }
 
-for (var i = 0; i < cards.length; i++){
-    cards[i].addEventListener("click", cardFunction);
+
+    if (moves > 8 && moves < 12) {
+        for (i = 0; i < 3; i++) {
+            if (i > 1) {
+
+            }
+        }
+    }
+    else if (moves > 13) {
+        for (i = 0; i < 3; i++) {
+            if (i > 0) {
+
+            }
+        }
+    }
 }
 
 
-/* set up the event listener for a card. */ 
 
- cards.onclick = function(event){
-     this.classList.toggle('open');
-     this.classList.toggle('show');
+var second = 0,
+    minute = 0;
+var interval;
 
- };
+function startTimer() {
+    interval = setInterval(function() {
+        timer.innerHTML = minute + "mins " + second + "secs";
+        second++;
+        if (second == 60) {
+            minute++;
+            second = 0;
+        }
+        if (minute == 60) {
+            minute = 0;
+        }
+    }, 1000);
+}
 
+
+
+function congratulations() {
+    if (matchedCard.length == 16) {
+        clearInterval(interval);
+        finalTime = timer.innerHTML;
+
+        // show congratulations modal
+        modal.classList.add("show");
+
+document.getElementById("finalMove").innerHTML = moves;
+
+document.getElementById("totalTime").innerHTML = finalTime;
+
+        
+
+        //closeicon on modal
+        closeModal();
+    }
+}
+
+
+function closeModal() {
+    closeicon.addEventListener("click", function(e) {
+        modal.classList.remove("show");
+        startGame();
+    });
+}
+
+
+function playAgain() {
+    modal.classList.remove("show");
+    startGame();
+}
+
+
+for (var i = 0; i < cards.length; i++) {
+    card = cards[i];
+    card.addEventListener("click", displayCard);
+    card.addEventListener("click", cardOpen);
+    card.addEventListener("click", congratulations);
+}
